697820014f3c3dd64b6c2e8af10a805e738e502e
Dasol Park <dasol.park615@gmail.com>
2018-10-23 00:16:22 +0900

    add get menu list, delete menu

--- app_food.js
+++ app_food.js
@@ -4,7 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var static = require('serve-static');
 var mongoose = require('mongoose');
-
+var Schema = mongoose.Schema;
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,22 +14,23 @@ app.use('/public', static(path.join(__dirname, 'public')));
 var database;
 var CafeteriaSchema;
 var CafeteriaModel;
-
+//메뉴 추가
 app.post("/process/addMenu", function(req, res){
-  console.log('/process/addMenu 호출됨.');
+  console.log('/process/addMenu 호출되었음.');
 
   var paramDate = req.body.date;
   var paramPart = req.body.part;
   var paramMenu = req.body.menu;
 
-  addMenu(database, paramDate, paramPart, paramMenu);
-  console.log('add Menu Success!');
+  addMenu(paramDate, paramPart, paramMenu);
+  console.log('Add Menu Success!');
 });
-function addMenu(database, paramDate, paramPart, paramMenu){//database 빼기
+function addMenu(paramDate, paramPart, paramMenu){
   var cafeteria = new CafeteriaModel({
-    "date":paramDate,
-    "part":paramPart,
-    "menu":paramMenu}
+      'date':paramDate,
+      'part':paramPart,
+      'menu':paramMenu
+      }
     );
 
   cafeteria.save(function(err) {
@@ -40,117 +41,46 @@ function addMenu(database, paramDate, paramPart, paramMenu){//database 빼기
     console.log(paramDate+ ', '+paramPart+ ', '+ paramMenu+ 'document 추가되었습니다.');
   });
 }
-
+//메뉴 읽기
 app.get("/process/listMenu", function(req, res){
-	req.app.get(database);
-	listMenu(database);
-	console.log('getList Success');
+  console.log('/process/listMenu 호출되었음.');
+	listMenu(req, res);
+	console.log('List Menu Success');
 });
-function listMenu(database) {//paramdate, parampart 를 이용해 삭제
-  console.log('listMenu 호출됨.');
-  console.log(database);
-  //메뉴 번호를 입력하면 해당 메뉴의 도큐먼트만 삭제되도록 할 것  
-  database.cafeteria.find({});
-  cafeteria.save(function(err, result) {
+function listMenu(req, res) {
+  CafeteriaModel.find({}, function(err, cafeterias){
+    console.log('cafeterias.length: '+cafeterias.length);
     if (err) {
       callback(err, null);
       return;
     } else {
-    	console.log(result);
-    	response.writeHead(200, {"Content-Type":"text/html"});
-    	response.end(JSON.stringify(result));
+    	res.writeHead(200, {"Content-Type":"text/html"});
+    	res.end(JSON.stringify(cafeterias));
     }
   });
 }
-/*
-app.use("/member/detail", function(request, response){
-	console.log(request.query);
-	var member2_id=request.query.member2_id;
+//메뉴 삭제
+app.post("/process/delMenu", function(req, res){
+  var paramDate = req.body.date;
+  var paramPart = req.body.part;
 
-	pool.getConnection(function(error, con){
-		if(error){
-			console.log(error);
-		}else{
-			var sql="select * from member2 where member2_id=?";
-			con.query(sql, [member2_id], function(err, result, fields){
-				if(err){
-					console.log(err);
-				}else{
-					console.log(result);
-					response.writeHead(200, {"Content-Type":"text/html"});
-					response.end(JSON.stringify(result));
-				}
-				con.release();
-			})
-		}
-	});
+  CafeteriaModel.remove({
+    'date': paramDate,
+    'part': paramPart
+    }, function(err, cafeterias){
+    console.log(cafeterias[0]+'삭제');
+  });
 });
 
-app.get("/member/del", function(request, response){
-	var member2_id=request.query.member2_id;
+//파일로 만들기
 
-	pool.getConnection(function(error, con){
-		if(error){
-			console.log(error);
-		}else{
-			var sql="delete from member2 where member2_id=?";
-			con.query(sql, [member2_id], function(err, result, fields){
-				if(err){
-					console.log(err, "삭제 실패ㅜㅜ");
-					response.writeHead(500, {"Content-Type":"text/html"});
-					response.end("fail");
-				}else{
-					console.log("삭제 결과", result);
-					response.writeHead(200, {"Content-Type":"text/html"});
-					if(result.affectedRows==1){
-						response.end("ok");
-					}else{
-						response.end("fail");
-					}
-				}
-				con.release();
-			});
-		}
-	});
-});
-
-app.post("/member/edit", function(request, response){
-	var member2_id=request.body.member2_id;
-	var id=request.body.id;
-	var pw=request.body.pw;
-	var name=request.body.name;
-
-	console.log(id, pw, name, member2_id);
-
-	pool.getConnection(function(error, con){
-		if(error){
-			console.log(error);
-		}else{
-			var sql="update member2 set id=?, pw=?, name=?, where member2_id=?";
-			con.query(sql, [id, pw, name, member2_id], function(err, result){
-				if(err){
-					console.log(err);
-				}else{
-					response.writeHead(200, {"Content-Type":"text/html"});
-					if(result.affectedRows==1){
-						response.end("ok");
-					}else{
-						response.end("fail");
-					}
-				}
-				con.release();
-			});
-		}
-	});
-});
-*/
 function connectDB() {
   // 데이터베이스 연결 정보
   var databaseUrl = 'mongodb://localhost:27017/local';
 
   // 데이터베이스 연결
   console.log('데이터베이스 연결을 시도합니다.');
-  mongoose.Promise = global.Promise;  // mongoose의 Promise 객체는 global의 Promise 객체 사용하도록 함
+  mongoose.Promise = global.Promise;
   mongoose.connect(databaseUrl);
   database = mongoose.connection;
 
@@ -158,11 +88,9 @@ function connectDB() {
   database.on('open', function () {
     console.log('데이터베이스에 연결되었습니다. : ' + databaseUrl);
 
-    // user 스키마 및 모델 객체 생성
     createCafeteriaSchema();
   });
 
-  // 연결 끊어졌을 때 5초 후 재연결
   database.on('disconnected', function() {
     console.log('연결이 끊어졌습니다. 5초 후 재연결합니다.');
     setInterval(connectDB, 5000);
@@ -170,22 +98,19 @@ function connectDB() {
 }
 
 function createCafeteriaSchema() {
-  // 스키마 정의
-  CafeteriaSchema = mongoose.Schema({
+
+  CafeteriaSchema = new Schema({
     date: {type: Number},
     part: {type: String, default: 0},
     menu: {type: String, default: 0},
   });
-  console.log('CafeteriaSchema 정의함.');
+  console.log('CafeteriaSchema 정의되었음');
 
-  // CafeteriaModel 모델 정의
-  CafeteriaModel = mongoose.model("Cafeteria", CafeteriaSchema);
-  app.set('database', database);
-  console.log('Cafeteria 정의함.');
+  CafeteriaModel = mongoose.model('Cafeteria', CafeteriaSchema);
+  console.log('CafeteriaModel 정의되었음');
 }
 
 http.createServer(app).listen(9999, function(){
   console.log('서버가 시작되었습니다. 포트 : ' + '9999');
-  // 데이터베이스 연결을 위한 함수 호출
   connectDB();
 });
---
+++ mongoose_example.js
@@ -0,0 +1,40 @@
+/** mongoose CRUD example **/
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost:27017/mongotest');
+mongoose.connection.on('open', function(){
+  console.log('Mongoose connected.');
+});
+
+var Account = new Schema({//Account에는 Schema 객체
+  username: { type: String },
+  date_created: { type: Date, default: Date.now },
+  visits: { type: Number, default: 0 },
+  active: { type: Boolean, default: false}
+});
+var AccountModel = mongoose.model('Account', Account);
+//AccountModel에는 모델 만드는 함수(function medel(doc, fields, skipId){}
+
+//추가하는 방법(Create document)
+//var newUser = new AccountModel({ username: 'randomUser1' }); //newUser에는 도큐먼트 한 줄이 있음
+//newUser.save();
+
+//읽어오는 방법(Read document)
+//AccountModel.find({}, function(err, accounts){
+//  console.log(accounts.length);
+//});
+
+//수정하는 방법(Update document)
+//var query = { username: 'randomUser1' };
+//AccountModel.findOneAndDelete(query, { username: 'randomUser2' }  function(err, accounts){
+//  console.log(accounts.length);
+//});
+
+//삭제하는 방법(Delete document)
+//AccountModel.remove({ username: 'randomUser1' }, function(err, accounts){
+//  console.log(accounts.length);
+//});
+
+//몽구스 종료(close mongoose)
+//  mongoose.connection.close();
